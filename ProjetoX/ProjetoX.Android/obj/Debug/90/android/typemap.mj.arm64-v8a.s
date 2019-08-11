7b7dc522f9ed170e1b12a935e07cc86ee0994efa
grmvata <53978714+grmvata@users.noreply.github.com>
2019-08-10 21:43:50 -0300

    Versao macaco
    Essa versao nao saiu exatamente como o esperado mas funcionou.

---
+++ ProjetoX/ProjetoX.Android/obj/Debug/90/android/typemap.mj.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: b26d9629e655cdf231295711a7f897fde6fa35a6 */
+	.arch	armv8-a
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1491
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 390643
+	.include	"typemap.mj.inc"
--- ProjetoX/ProjetoX/App.xaml.cs
+++ ProjetoX/ProjetoX/App.xaml.cs
@@ -9,23 +9,9 @@ namespace ProjetoX
         public App()
         {
             InitializeComponent();
-
             MainPage = new MainPage();
         }
 
-        protected override void OnStart()
-        {
-            // Handle when your app starts
-        }
-
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }
-
-        protected override void OnResume()
-        {
-            // Handle when your app resumes
-        }
+    
     }
 }
